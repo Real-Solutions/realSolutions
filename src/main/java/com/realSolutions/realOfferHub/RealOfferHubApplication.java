01dce65c5d5b90e1d1687908a1e264fbd6025298
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-11-17 18:00:30 -0500

    added files

---
+++ src/main/java/com/realSolutions/realOfferHub/RealOfferHubApplication.java
@@ -0,0 +1,13 @@
+package com.realSolutions.realOfferHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealOfferHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealOfferHubApplication.class, args);
+	}
+
+}
