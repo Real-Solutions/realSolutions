1aa52ef1e9971845607038c80bf9a2f1f7999c4b
Austin Limanek <austin.limanek@gmail.com>
2022-11-18 15:35:30 -0800

    added the ability to provide offers on properties

---
+++ src/main/java/com/realSolutions/realOfferHub/controllers/OfferController.java
@@ -0,0 +1,56 @@
+package com.realSolutions.realOfferHub.controllers;
+
+import com.realSolutions.realOfferHub.models.Offer;
+import com.realSolutions.realOfferHub.models.Property;
+import com.realSolutions.realOfferHub.models.SiteUser;
+import com.realSolutions.realOfferHub.repositories.OfferRepository;
+import com.realSolutions.realOfferHub.repositories.PropertyRepository;
+import com.realSolutions.realOfferHub.repositories.SiteUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+import java.util.ArrayList;
+
+@Controller
+public class OfferController {
+
+    @Autowired
+    SiteUserRepository siteUserRepository;
+
+    @Autowired
+    PropertyRepository propertyRepository;
+
+    @Autowired
+    OfferRepository offerRepository;
+
+    @GetMapping("/offer")
+    public String getOffer(Principal p, Model m){
+        SiteUser agent = siteUserRepository
+                .findByUsername(p.getName());
+        m.addAttribute("username", p.getName());
+        ArrayList<String> addresses = new ArrayList<>();
+        for (SiteUser seller : agent.getSellers()){
+            for(Property property : seller.getProperties()){
+                addresses.add(property.getAddress());
+            }
+        }
+        m.addAttribute("addresses", addresses);
+        return "offer";
+    }
+
+    @PostMapping("/offer")
+    public RedirectView newOffer(String address, String price, String downPayment, String contingentBuyer){
+        Property property = propertyRepository.getPropertyByAddress(address);
+        float pricef = Float.parseFloat(price);
+        float downPaymentf = Float.parseFloat(downPayment);
+        boolean contingentBuyerb = !contingentBuyer.equals("no");
+        Offer newOffer = new Offer(address, pricef, downPaymentf, contingentBuyerb, property);
+        offerRepository.save(newOffer);
+        return new RedirectView("/dashboard");
+    }
+}
--- src/main/java/com/realSolutions/realOfferHub/models/Message.java
+++ src/main/java/com/realSolutions/realOfferHub/models/Message.java
@@ -1,4 +1,43 @@
 package com.realSolutions.realOfferHub.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String body;
+    private Date date;
+
+    @ManyToOne
+    Offer myOffer;
+
+    public Message() {
+    }
+
+    public Message(String body, Date date, Offer myOffer) {
+        this.body = body;
+        this.date = date;
+        this.myOffer = myOffer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Offer getMyOffer() {
+        return myOffer;
+    }
 }
